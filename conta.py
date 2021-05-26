e8a6955e1d84e5e81605f22a4cc20e43c14961b8
Luiz Otavio Amboni <58530274+luiz-amboni@users.noreply.github.com>
2021-05-26 14:17:27 -0300

    Adicionado complementações finais do curso de introdução orientação a objeto

--- conta.py
+++ conta.py
@@ -13,8 +13,16 @@ class Conta:
     def deposita(self, valor):
         self.__saldo += float(valor)
 
+    def __pode_sacar(self, valor_saque):
+        valor_disponivel_saque = self.__saldo + self.__limite
+        return valor_saque <= valor_disponivel_saque
+
     def saca(self, valor):
-        self.__saldo -= float(valor)
+        if(self.__pode_sacar(valor)):
+            self.__saldo -= float(valor)
+            print("Você sacou R${}. O valor atual da sua conta é de R${}".format(valor, self.__saldo))
+        else:
+            print("O valor R${} passou o limite".format(valor))
 
     def tranfere(self, valor, destino):
         self.saca(float(valor))
@@ -32,7 +40,17 @@ class Conta:
     def limite(self):
         return self.__limite
 
+    @staticmethod
+    def codigo_banco():
+        return "001"
+
+    @staticmethod
+    def codigos_bancos():
+        return {'BB': '001', 'Caixa': '104', 'Bradesco': '237'}
+
     @limite.setter
     def limite(self, limite):
         print("Limite atualizado de R${} para R${}".format(self.__limite, limite))
         self.__limite = limite
+
+
