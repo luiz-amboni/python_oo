efef5b4cc57c47c1fb1a14f8e82535e8a190b76f
Luiz Otavio Amboni <58530274+luiz-amboni@users.noreply.github.com>
2021-05-26 12:10:41 -0300

    Adicionado métodos get/set e utilização de @property.

--- conta.py
+++ conta.py
@@ -20,3 +20,19 @@ class Conta:
         self.saca(float(valor))
         destino.deposita(float(valor))
 
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        print("Limite atualizado de R${} para R${}".format(self.__limite, limite))
+        self.__limite = limite
