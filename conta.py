cde6a7cfbf4f9d80a8574b3b730f2dd83e87e598
Luiz Otavio Amboni <58530274+luiz-amboni@users.noreply.github.com>
2021-05-26 00:36:57 -0300

    Adicionado arquivos referente a aula de classes

---
+++ conta.py
@@ -0,0 +1,9 @@
+
+class Conta:
+
+    def __init__(self, numero, titular, saldo, limite):
+        print("Contruindo objeto ... {}".format(self))
+        self.numero = numero
+        self.titular = titular
+        self.saldo = float(saldo)
+        self.limite = float(limite)
--- teste.py
+++ teste.py
@@ -1,12 +1,13 @@
 def cria_conta(numero, titular, saldo, limite):
-    conta = {"numero": numero, "titular": titular, "saldo": saldo, "limite": limite}
+    conta = {"numero": numero, "titular": titular, "saldo": float(saldo), "limite": float(limite)}
     return conta
 
 def deposita(conta, valor):
     conta["saldo"] += valor
-2
+
 def saca(conta, valor):
     conta["saldo"] -= valor
 
 def extrato(conta):
     print("saldo {}".format(conta["saldo"]))
+
